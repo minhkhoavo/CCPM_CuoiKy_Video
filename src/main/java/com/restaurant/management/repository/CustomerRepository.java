ba0998f180d7dca17f24594e8d8709d16fad4120
minhkhoavo <v.minhkhoa123456@gmail.com>
2024-11-02 10:49:30 +0700

    fix: layout temple thymeleaef and and customer service, repo, controller

---
+++ src/main/java/com/restaurant/management/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.restaurant.management.repository;
+
+import com.restaurant.management.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
